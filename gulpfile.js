3e1e09d97ff9cacaa44018a86d1c71c07fed25e9
jmedley@google.com <jmedley@google.com>
2016-05-02 12:52:44 -0700

    Add a service worker.

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ gulp.task('make-service-worker', function(callback) {
 	var rootDir = 'app';
 
 	swPrecache.write(path.join(rootDir, 'serviceworker.js'), {
-		staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif}'],
+		staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif}',],
 		stripPrefix: rootDir
 	}, callback);
 });
