1f3621adfcd36e6741b105e3df6d6dfe247a81d2
jmedley@google.com <jmedley@google.com>
2016-05-02 12:48:16 -0700

    Add a gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var path = require('path');
+var swPrecache = require('sw-precache');
+
+gulp.task('make-service-worker', function(callback) {
+	var rootDir = 'app';
+
+	swPrecache.write(path.join(rootDir, 'serviceworker.js'), {
+		staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif}'],
+		stripPrefix: rootDir
+	}, callback);
+});
